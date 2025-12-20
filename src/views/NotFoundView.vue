44a76ca2c15ae9db678f62e9fdf9334ce7de43fe
Shamiul <anik.savar.bd@gmail.com>
2025-12-21 01:04:22 +0900

    design update: not found page text color update

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -3,7 +3,7 @@
     <div class="text-center">
       <h1 class="text-6xl font-bold text-red-600 mb-4">404</h1>
       <h2 class="text-3xl font-semibold text-red-600 mb-4">Page Not Found</h2>
-      <p class="text-md md:text-xl text-gray-600 mb-8">
+      <p class="text-md md:text-xl text-red-600 mb-8">
         The page you are looking for does not exist.
       </p>
       <!-- <RouterLink
