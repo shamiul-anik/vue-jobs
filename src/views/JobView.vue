9b88ca3066c72357aeca089012d0a58fbee04005
Shamiul <anik.savar.bd@gmail.com>
2025-12-24 20:02:43 +0900

    feature removed: download job posting as PDF feature removed as the feature doesn't support the new version of TailwindCSS

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -77,29 +77,22 @@
 
             <hr class="border-gray-200 my-6">
 
-            <div class="flex justify-end gap-3">
-              <RouterLink v-if="isUser"
-                :to="`/job-pdf/${job.id}`"
-                class="bg-green-600 hover:bg-green-700 text-white px-4 py-1.5 rounded-lg cursor-pointer text-md transition-all duration-300">
-                <i class="fas fa-file-pdf mr-1"></i>
-                Download PDF
-              </RouterLink>
-
-              <RouterLink v-if="isAdmin"
+            <div v-if="isAdmin" class="flex justify-end gap-3">
+              <RouterLink
                 :to="`/edit-job/${job.id}`"
                 class="bg-green-600 hover:bg-green-700 text-white px-4 py-1.5 rounded-lg cursor-pointer text-md transition-all duration-300">
                 <i class="fas fa-edit mr-1"></i>
                 Edit Job
               </RouterLink>
-              <button v-if="isAdmin"
+              <button
                 @click="handleDelete"
                 class="bg-red-600 hover:bg-red-700 text-white px-4 py-1.5 rounded-lg cursor-pointer text-md transition-all duration-300">
                 <i class="fas fa-trash-alt mr-1"></i>
                 Delete Job
               </button>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
     </section>
@@ -133,10 +126,6 @@ const { user } = useAuth()
 const job = ref({})
 const loading = ref(true)
 const error = ref(null)
-
-const isUser = computed(() => {
-  return user.value && user.value.role === 'user'
-})
 
 const isAdmin = computed(() => {
   return user.value && user.value.role === 'admin'
